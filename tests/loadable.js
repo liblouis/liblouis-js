6968d8924195bcf4a7fbf06f6deb77bdb12e9082
Reiner Dolp <reiner-dolp@users.noreply.github.com>
2017-03-25 10:08:47 +0100

    Removes mock tests and unnecessary output in travis

--- tests/loadable.js
+++ tests/loadable.js
@@ -2,10 +2,3 @@ var testrunner = require("../testrunner/testrunner");
 testrunner.groupStart("rudimentary functional tests");
 
 var assert = testrunner.assert;
-
-assert("test", true);
-assert("test 2", true);
-assert("test 3", false);
-throw new Error("exception msg");
-assert("test 4", true);
-assert("test 4", true);
