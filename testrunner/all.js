73032f867192de1c4a19b03ef5030926e0ba6d85
Reiner Dolp <reiner-dolp@users.noreply.github.com>
2017-04-25 18:31:28 +0100

    fix race condition between test runners

--- testrunner/all.js
+++ testrunner/all.js
@@ -1,2 +1,7 @@
-require("./browser");
-require("./node");
+var execSync = require('child_process').execSync;
+var path = require('path');
+
+process.chdir(path.resolve(__dirname, ".."));
+
+execSync("node ./testrunner/browser", {stdio:'inherit'});
+execSync("node ./testrunner/node", {stdio:'inherit'});
